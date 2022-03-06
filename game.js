a229471060a5338542c98c40b268178dc764cf9f
unknown <rajat.jaiswal@indexnine.com>
2022-03-06 18:54:09 +0530

    changes are made according to sushil and also added few changes of my own♥

--- game.js
+++ game.js
@@ -1,27 +1,73 @@
-const options = ["rock", "paper", "scissors"];
-const winners = [];
-
-function computerPlay(){
-    return options[Math.floor(Math.random() * options.length)];
-}
-
-function game(){
-    //for playing the game for multiple rounds
-    for(let i=1;i<=5;i++){
-        playRound();
+let [computer_score,user_score]=[0,0];
+let result_ref=document.getElementById("result");
+let choicesObject = {
+    'rock' : {
+        'rock' : 'tie',
+        'scissor' : 'win',
+        'paper' : 'lose'
+    },
+    'scissor' : {
+        'rock' : 'lose',
+        'scissor' : 'tie',
+        'paper' : 'win'
+    },
+    'paper' : {
+        'rock' : 'win',
+        'scissor' : 'lose',
+        'paper' : 'tie'
     }
-    logWins();
 }
 
-function playRound(){
-    const playerSelection = playerPlay();
-    const computerSelection = computerPlay();
-    const winner=checkWinner(playerSelection,computerSelection);
-    winners.push(winner);
+let options = ["rock", "paper", "scissor"];
 
+function checker(input){
+    //Round result details
+    let CP = options[Math.floor(Math.random() * options.length)];
+
+    document.getElementById("comp_choice").innerHTML =
+    `Computer choose <span> ${CP.toUpperCase()} </span>`;
+
+    document.getElementById("user_choice").innerHTML =
+    `You choose <span> ${input.toUpperCase()} </span>`;
+    console.log("Computer:"+CP+" vs "+"you:"+input);
+
+    switch(choicesObject[input][CP]){
+        case "win":     
+                    result_ref.innerHTML = "YOU WIN";
+                    user_score++;
+                    break;
+        case "lose":
+                    result_ref.innerHTML = "YOU LOSE";
+                    computer_score++;
+                    break;
+        case "tie": // default: 
+                    result_ref.innerHTML = "TIE";
+                    break;
+    }
+
+    document.getElementById("computer_score").innerHTML = computer_score;
+    document.getElementById("user_score").innerHTML = user_score;
 }
 
-function playerPlay(){
+/*function checkWinner(choiceP,choiceC){
+    if(choiceP == choiceC){
+        return "Tie";
+    }else if(   (choiceP === "rock" && choiceC == "scissors") ||
+                (choiceP === "paper" && choiceC == "rock") ||
+                (choiceP === "scissors" && choiceC == "paper")  ){
+                    console.log("You Win!");
+                return "Player";
+            }else{ 
+                    console.log("Computer Win!");
+                    return "Computer"; 
+                }
+}
+
+function logWins(){
+    console.log(winners);
+}
+game();
+/*function playerPlay(){
     let input = prompt("Type Rock, Paper or Scissors:");
     while(input==null){
         input=prompt("Type Rock, Paper or Scissors:");
@@ -43,27 +89,21 @@ function playerPlay(){
 function validInput(option){
     return options.includes(option);    
 }
-
-
-
-
-function checkWinner(choiceP,choiceC){
-    if(choiceP == choiceC){
-        return "Tie";
-    }else if(   (choiceP === "rock" && choiceC == "scissors") ||
-                (choiceP === "paper" && choiceC == "rock") ||
-                (choiceP === "scissors" && choiceC == "paper")  ){
-                    console.log("You Win!");
-                return "Player";
-            }else{ 
-                    console.log("Computer Win!");
-                    return "Computer"; 
-                }
+function game(){
+    //for playing the game for multiple rounds
+    //for(let i=1;i<=5;i++){
+        playRound();
+   // }
+    //logWins();
 }
 
-function logWins(){
-    let pla
-    console.log(winners);
+function playRound(){
+    const playerSelection = playerPlay();
+    const computerSelection = computerPlay();
+    const winner=checkWinner(playerSelection,computerSelection);
+    winners.push(winner);
+ const winners = [];
+function computerPlay(){
+    return options[Math.floor(Math.random() * options.length)];
 }
-
-//game();
+}*/
